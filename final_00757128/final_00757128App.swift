cdb7a7198c7cfeab87280dbb3a83f58d492aff7c
as468579 <as468579@gmail.com>
2020-12-16 15:16:52 +0800

    Initial Commit

---
+++ final_00757128/final_00757128App.swift
@@ -0,0 +1,20 @@
+//
+//  final_00757128App.swift
+//  final_00757128
+//
+//  Created by User18 on 2020/12/16.
+//
+
+import SwiftUI
+
+@main
+struct final_00757128App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
